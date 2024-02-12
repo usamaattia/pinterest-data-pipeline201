ac73da7040421b9c9b1bf6a0fee025f79c0f37e3
usamaattia <usamaalatiyah9@gmail.com>
2024-02-12 16:32:06 +0000

    creating a tamplate to create the DAG

---
+++ 1209b9ad90a5_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/usamaalatiyah9@gmail.com/Untitled Notebook 2024-02-03 13:36:22',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'osama',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('1209b9ad90a5_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 1, 1),
+    # check out possible intervals, should be a string
+    schedule='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
